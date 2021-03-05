c4fec5176947c7e2ae23a7c114104e86956fa01b
tisontang <tisontang@tencent.com>
2021-03-05 16:26:24 +0800

    fix: 修复切换函数式丢失params问题

--- src/components/rich-prop-editor/rich-prop-editor.jsx
+++ src/components/rich-prop-editor/rich-prop-editor.jsx
@@ -17,9 +17,6 @@ export function RichPropEditor(props) {
       <Label>{label}</Label>
       <Select width="90px" options={items} value={data.type} onChange={(e) => onChange(produce(data, data => {
         data.type = +e.target.value
-        if (data.type !== VALUE_TYPES.FN) {
-          data.params = ''
-        }
       }))} disabled={data.disabled} />
       {data.type === VALUE_TYPES.FN ? <Input width="90px" placeholder="参数" value={data.params} onChange={(e) => onChange(produce(data, data => { data.params = e.target.value }))} disabled={data.disabled} /> : null}
       {data.type === VALUE_TYPES.STR ? <Input value={data.value} onChange={(e) => onChange(produce(data, data => { data.value = e.target.value }))} disabled={data.disabled} /> : null}
